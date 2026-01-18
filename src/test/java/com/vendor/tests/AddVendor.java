4da0b8ffac50bf39fae6b3394f04a599e28724ac
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2026-01-18 14:33:49 +0530

    Implement addVendorTest method in AddVendor class
    Added a test case for adding a vendor using RestAssured.

--- src/test/java/com/vendor/tests/AddVendor.java
+++ src/test/java/com/vendor/tests/AddVendor.java
@@ -1,5 +1,37 @@
 package com.vendor.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class AddVendor {
 
+	@Test
+	public void addVendorTest() throws JSONException {
+		RestAssured.baseURI = "http://10.81.69.76:8080/RestDemo5/api";
+		RestAssured.basePath = "/vendors";
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("vendorId", "Vendor537");
+		jsonObject.put("vendorName", "LG");
+		jsonObject.put("vendorAddress", "Mysuru");
+		jsonObject.put("vendorPhoneNumber", "1010101010");
+
+		request.body(jsonObject.toString());
+		Response response = request.post();
+
+		String responseBody = response.getBody().asString();
+		System.out.println("Response Body: " + responseBody);
+
+		Assert.assertEquals(response.getStatusCode(), 201, "Status code check failed!");
+		Assert.assertTrue(response.getStatusLine().contains("201"), "Status line check failed!");
+		Assert.assertTrue(responseBody.contains("InfosysMysuru"), "Vendor Name mismatch!");
+		Assert.assertTrue(responseBody.contains("Mysuru"), "Address mismatch!");
+		Assert.assertTrue(responseBody.contains("1010101010"), "Phone Number mismatch!");
+	}
 }
