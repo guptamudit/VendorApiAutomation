26644c89fcab55c40a17fccf4a1c36be5d09f236
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2026-01-18 14:33:14 +0530

    Implement VendorSteps for API testing

--- src/test/java/com/vendor/stepDefinitions/VendorSteps.java
+++ src/test/java/com/vendor/stepDefinitions/VendorSteps.java
@@ -1,5 +1,32 @@
 package com.vendor.stepDefinitions;
 
-public class VendorSteps {
+import org.testng.Assert;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class VendorSteps {
+	Response response;
+	String vendorName;
+
+	@Given("The user initiates a GET request to the Vendor URL")
+	public void the_user_initiates_a_GET_request_to_the_Vendor_URL() {
+		RestAssured.baseURI = "http://10.81.69.76:8080/RestDemo5/api";
+		RestAssured.basePath = "/vendors";
+		response = RestAssured.given().queryParam("vendorId", "Vendor1").when().get();
+	}
+
+	@When("The user Fetches the VendorName from the response")
+	public void the_user_Fetches_the_VendorName_from_the_response() {
+		vendorName = response.jsonPath().getString("vendorName");
+	}
+
+	@Then("validate that the Vendor Name is IBM and status code {int}")
+	public void validate_that_the_Vendor_Name_is_IBM_and_status_code(Integer int1) {
+		Assert.assertEquals(response.getStatusCode(), 200);
+		Assert.assertEquals(vendorName, "IBM");
+	}
 }
